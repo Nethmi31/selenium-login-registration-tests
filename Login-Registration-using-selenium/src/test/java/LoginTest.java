7f2948733bba99d7b4fab8104d7e1d85557cd6a3
Nethmi Rashmika <h.u.g.nethmirashmika@gmail.com>
2025-04-18 23:05:23 +0530

    up

--- Login-Registration-using-selenium/src/test/java/LoginTest.java
+++ Login-Registration-using-selenium/src/test/java/LoginTest.java
@@ -3,17 +3,27 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
     WebDriver driver;
-    @Test
+    @BeforeMethod
     public void setup()
     {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.google.lk/");
-        driver.findElement(By.name("q")).sendKeys("daraz.lk" + Keys.ENTER);
+        driver.findElement(By.name("q")).sendKeys("thilakawardhana.lk" + Keys.ENTER);
+
+    }
+
+    //Redirected to the dashboard page
+    @Test
+    public void MoveDashboard(){
+        WebElement HomeLink = driver.findElement(By.xpath("/html/body/div[3]/div/div[11]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/div[2]/div/div/span/a"));
+        HomeLink.click();
     }
 }
 
