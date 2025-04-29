9d7268b9dd494d13ea7653359270106ae711a648
Nethmi Rashmika <h.u.g.nethmirashmika@gmail.com>
2025-04-29 21:23:47 +0530

    update

--- Login-Registration-using-selenium/src/test/java/LoginTest.java
+++ Login-Registration-using-selenium/src/test/java/LoginTest.java
@@ -10,11 +10,11 @@ public class LoginTest {
 
     WebDriver driver;
     @BeforeMethod
-    public void setup()
-    {
+    public void setup() throws InterruptedException {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://thilakawardhana.com/");
+        Thread.sleep(3000);
     }
 
     //Redirected to the user icon
@@ -22,14 +22,12 @@ public class LoginTest {
     public void findusericon(){
         WebElement account = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/sticky-header[1]/header[1]/div[1]/div[2]/div[1]/a[1]/*[name()='svg'][1]"));
         account.click();
-    }
 
-    //Happy path for the login.
-    public void happypath(){
         WebElement email=driver.findElement(By.id("customer_email"));
         email.sendKeys("h.u.g.nethmirashmika@gmail.com");
         WebElement password =  driver.findElement(By.id("customer_password"));
         password.sendKeys("1234");
+        
     }
 
 }
